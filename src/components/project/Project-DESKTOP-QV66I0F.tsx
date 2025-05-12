a37db69934e800a74409cb0bb169574282921584
zarathmgd <zarathmougamadou@gmail.com>
2025-05-12 18:36:13 +0200

    initial commit

--- src/components/project/Project-DESKTOP-QV66I0F.tsx
+++ src/components/project/Project-DESKTOP-QV66I0F.tsx
@@ -7,20 +7,38 @@ const projects = [
   {
     projectName: "Outil Check ODB / DTime",
     projectInformations: "Le projet consiste à vérifier l'exactitude des données entre la nouvelle application de gestion des ressources humaines, DTime, et la base de données ODB à Disneyland Paris. Les données transitent par des systèmes ETL, nécessitant des tests pour confirmer leur conformité. L'équipe BFS a développé un outil pour faciliter ces tests, réalisés par des analystes financiers, en se basant sur des scénarios de dates et d'identifiants d'employés.",
-    compteRenduLink: "public/Compte rendu Outil ODB DTime.pdf",
-    modeOperatoireLink: "public/MODE OPERATOIRE Check DTime-ODB_V2.0.pdf",
-    image: "public/excel-microsoft-2013-application-logo-oo7x8r2rnik4ohlo.jpg",
-    skills: [{id: 0, name: "Excel"}],
+    link: "Compte rendu Outil ODB DTime.pdf",
     category: "Entreprise",
   },
   {
-    projectName: "Outil Envoi AOP KNIME",
-    projectInformations: "Le projet vise à automatiser l'envoi de l'AOP (Annual Operating Plan) en utilisant KNIME. Auparavant réalisé manuellement via Excel, ce processus est désormais optimisé pour traiter et préparer les données budgétaires annuelles de manière plus efficace et fiable. L'outil KNIME lit les fichiers Excel, effectue les transformations nécessaires, et génère des fichiers prêts à être chargés dans les systèmes BW et ODB, réduisant ainsi le temps et les erreurs.",
-    compteRenduLink: "public/Compte rendu Outil Envoi AOP.pdf",
-    modeOperatoireLink: "public/MODE OPERATOIRE Outil Envoi AOP KNIME.pdf",
-    image: "public/knime_wallpaper.jpg",
-    skills: [{id: 0, name: "Excel"}, {id: 1, name: "KNIME"}],
+    projectName: "Project 2",
+    projectInformations: "NOTHING is an english perfume brand. In the website, you can find the story about the industry, the products that are proposed and more informations.",
+    link: "https://zarathmgd.github.io/nothing-react/",
+    category: "Personnel",
+  },
+  {
+    projectName: "Project 1",
+    projectInformations: "BY PIZZA is a pizza restaurant located in France. The website presents the company with its history, the menus offered by the restaurant and some informations about BY PIZZA.",
+    link: "https://zarathmgd.github.io/bypizza-react/",
     category: "Entreprise",
+  },
+  {
+    projectName: "Project 2",
+    projectInformations: "NOTHING is an english perfume brand. In the website, you can find the story about the industry, the products that are proposed and more informations.",
+    link: "https://zarathmgd.github.io/nothing-react/",
+    category: "Personnel",
+  },
+  {
+    projectName: "Project 1",
+    projectInformations: "BY PIZZA is a pizza restaurant located in France. The website presents the company with its history, the menus offered by the restaurant and some informations about BY PIZZA.",
+    link: "https://zarathmgd.github.io/bypizza-react/",
+    category: "Entreprise",
+  },
+  {
+    projectName: "Project 2",
+    projectInformations: "NOTHING is an english perfume brand. In the website, you can find the story about the industry, the products that are proposed and more informations.",
+    link: "https://zarathmgd.github.io/nothing-react/",
+    category: "Personnel",
   },
 ];
 
@@ -136,10 +154,7 @@ export default function Project() {
               <ProjectContent
                 projectName={project.projectName}
                 projectInformations={project.projectInformations}
-                compteRenduLink={project.compteRenduLink}
-                modeOperatoireLink={project.modeOperatoireLink}
-                projectSkills={project.skills}
-                image={project.image}
+                link={project.link}
               />
             </Grid>
           ))}
