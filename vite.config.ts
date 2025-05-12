3f0b32e187f19de03f6ea2f4b70bf973b99d7703
zarathmgd <zarathmougamadou@gmail.com>
2025-05-12 19:33:33 +0200

    Empty page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-BTSSIO/",
+  base: "https://zarathmgd.github.io/Portfolio-BTSSIO/",
   plugins: [react()],
 });
